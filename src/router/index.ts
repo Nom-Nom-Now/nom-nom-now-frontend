fab74e4bde01b06a3b30dae0d0e7606d0c75eede
Rafael Till <tillrafael@icloud.com>
2025-11-17 14:54:56 +0100

    feat(recipes): add component to list and search all recipes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HelloWorld from '../components/HelloWorld.vue';
-
+//import HelloWorld from '../components/HelloWorld.vue';
+import AllRecipesPage from '../components/feature/recipe/src/Pages/AllRecipesPage.vue';
 const routes = [
-  { path: '/', component: HelloWorld, props: { msg: 'Vite + Vue' } },
+  //{ path: '/', component: HelloWorld, props: { msg: 'Vite + Vue' } },
+  { path: '/', component: AllRecipesPage},
 ];
 
 const router = createRouter({
