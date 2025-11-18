835585c8335d86dfef2342764d3dad7df28a20a1
Tino <80458867+tinow04@users.noreply.github.com>
2025-11-18 09:22:41 +0100

    chore: implemented material web and made minor code changes (#7)
    [NNN-222](https://nomnomnow.atlassian.net/jira/software/projects/NNN/boards/1?selectedIssue=NNN-222)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,34 +7,46 @@ const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="screen">
+    <md-tabs>
+      <md-primary-tab>{{ msg }}</md-primary-tab>
+    </md-tabs>
+    <h1></h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <div class="card">
+      <md-filled-button type="button" @click="count++"
+        >count is {{ count }}</md-filled-button
+      >
+
+      <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test HMR
+      </p>
+    </div>
+
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      Check out
+      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+        >create-vue</a
+      >, the official Vue + Vite starter
     </p>
+    <p>
+      Learn more about IDE Support for Vue in the
+      <a
+        href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+        target="_blank"
+        >Vue Docs Scaling up Guide</a
+      >.
+    </p>
+    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
+.screen {
+  background-color: var(--md-sys-color-surface);
+}
+
 .read-the-docs {
   color: #888;
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import './style.css';
 import App from './App.vue';
 import router from './router';
 
+//material design
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/checkbox/checkbox.js';
+
 const app = createApp(App);
 
 app.use(router);
