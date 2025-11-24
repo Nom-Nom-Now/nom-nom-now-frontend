1a59edaaac24bb9d801e4ecc74665dc2c483af54
tinow04 <tino.wolter2@gmail.com>
2025-11-24 23:59:18 +0100

    feat(create-recipe): Added Page and individual title

--- src/components/Frame/TitleFrame.vue
+++ src/components/Frame/TitleFrame.vue
@@ -2,7 +2,7 @@
   <div>
     <md-tabs>
       <md-primary-tab>
-        <span class="tab-title"> {{ t('feature.mainPage.helloWorld') }} </span>
+        <span class="tab-title"> {{ pageTitle }} </span>
         <!-- TODO: Tab Title so anpassen, dass aktuelle Seite mitgegeben wird -->
       </md-primary-tab>
     </md-tabs>
@@ -10,8 +10,11 @@
 </template>
 
 <script setup lang="ts">
-import { useI18n } from 'vue-i18n';
-const { t } = useI18n();
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
+const route = useRoute();
+const pageTitle = computed(() => (route.meta.title as string) ?? 'Nom Nom Now');
 </script>
 
 <style scoped>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HelloWorld from '../components/HelloWorld.vue';
+import CreateRecipeView from '../feature/create-recipe/src/pages/CreateRecipeView.vue';
 
-const routes = [{ path: '/', component: HelloWorld, props: { msg: 'Vite + Vue' } }];
+const routes = [
+  { path: '/', component: HelloWorld, props: { msg: 'Vite + Vue' } },
+  { path: '/create', component: CreateRecipeView, meta: { title: 'Create Recipe' } }
+];
 
 const router = createRouter({
   history: createWebHistory('/nom-nom-now/'),
