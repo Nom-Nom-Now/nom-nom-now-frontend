4087de6b534fa342ab438ebe81ecf8ec54ca6341
Tino <80458867+tinow04@users.noreply.github.com>
2025-11-24 18:00:06 +0100

    feat(Frame): Added Frame Structure (#8)
    * feat(Frame): Frame added without function
    
    * feat(Frame): Added Typography and round Corner
    
    https://nomnomnow.atlassian.net/browse/NNN-224

---
+++ src/components/Frame/NavigationItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="item-container">
+    <img :src="iconSrc" />
+    {{ navItemProps.text }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const navItemProps = defineProps<{
+  text: string;
+  iconName: string;
+}>();
+
+const iconSrc = new URL(`../../assets/icons/${navItemProps.iconName}.svg`, import.meta.url).href;
+</script>
+
+<style scoped>
+.item-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 3.1875rem;
+  gap: 0.25rem;
+}
+</style>
---
+++ src/components/Frame/TitleFrame.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <md-tabs>
+      <md-primary-tab>
+        <span class="tab-title">Create Recipe</span>
+      </md-primary-tab>
+    </md-tabs>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+div {
+  height: 4.5rem;
+  background-color: var(--md-sys-color-surface);
+  padding-left: 2rem;
+
+  display: flex;
+  align-items: center;
+}
+
+.tab-title {
+  /* Wir nutzen die System-Variablen aus deiner Doku für "Title Large" */
+
+  font-size: var(--md-sys-typescale-title-large-size);
+  line-height: var(--md-sys-typescale-title-large-line-height);
+
+  /* "Emphasized" bedeutet in Material meistens ein höheres Font-Weight.
+     Der Standard Title-Large ist oft 400. Wir setzen ihn manuell hoch. */
+
+  /* WICHTIG: Damit die Farbe weiterhin vom Tab gesteuert wird (aktiv/inaktiv) */
+  color: var(--md-sys-color-on-surface-variant);
+}
+</style>
